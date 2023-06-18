2acc4c2b26d59384be1be2912fcbfbaf0bea9405
BaoTheTran <ttbao2710@gmail.com>
2023-06-18 13:49:27 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
